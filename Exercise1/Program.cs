e36c8b31521f23d6cfb47ce37e0e9c3b70ea64cb
DionFinnerty <dion@finnerty.id.au>
2021-03-18 22:59:42 +1100

    selection submittable task

---
+++ Exercise1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your age?");
+            string age = Console.ReadLine();
+            int userAge = int.Parse(age);
+
+            Console.WriteLine("are you male or female?");
+            string userGender = Console.ReadLine();
+
+            Console.WriteLine("enter a random number between 1-10!");
+            string userNum1 = Console.ReadLine();
+            int number1 = int.Parse(userNum1);
+
+            Console.WriteLine("enter another random number between 1-10!");
+            string userNum2 = Console.ReadLine();
+            int number2 = int.Parse(userNum2);
+
+            if (userAge > 17)
+            {
+                Console.WriteLine("Legally adult age");
+                if (userGender == "male")
+                {
+                    Console.WriteLine("Male is an adult");
+                }
+
+                if (userGender == "female")
+                {
+                    Console.WriteLine("Female is an adult");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Legally not adult age");
+                if (userGender == "male")
+                {
+                    Console.WriteLine("Male is not an adult");
+                }
+
+                if (userGender == "female")
+                {
+                    Console.WriteLine("Female is not an adult");
+                }
+            }
+
+            if (number2 + number1 > 10)
+            {
+                Console.WriteLine("Sum of numbers is greater than 10");
+            }
+            else
+
+            if (number2 + number1 < 10)
+            {
+                Console.WriteLine("Sum of numbers is less than 10");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            string appUsername = "dionfinnerty";
+            string appPassword = "speaker";
+
+            Console.WriteLine("please enter username");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("please enter your password!");
+            string userPassword = Console.ReadLine();
+            
+            if (username == appUsername )
+            {
+                if (userPassword == appPassword)
+                {
+                    Console.WriteLine("login successful");
+                }
+                else 
+                {
+                    Console.WriteLine("login unsuccessful");
+                }
+
+            }
+            else 
+            {
+            Console.WriteLine("login unsuccessful");
+            }
+            
+        }
+    }
+}
